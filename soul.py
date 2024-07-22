1ff9115558d78711216bb4f42a4a9298d32fe871
Finishermod <166985997+Finishermod@users.noreply.github.com>
2024-07-22 18:01:31 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -18,9 +18,9 @@ loop = asyncio.get_event_loop()
 
 TOKEN = '6906001988:AAGwJL92zsCakC8gGcCfLlJpGgyr9g42qVo'
 MONGO_URI = 'mongodb+srv://piroop:piroop@piro.hexrg9w.mongodb.net/?retryWrites=true&w=majority&appName=piro&tlsAllowInvalidCertificates=true'
-FORWARD_CHANNEL_ID = --1001357222529
-CHANNEL_ID = --1001357222529
-error_channel_id = --1001357222529
+FORWARD_CHANNEL_ID = -1001357222529
+CHANNEL_ID = -1001357222529
+error_channel_id = -1001357222529
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
